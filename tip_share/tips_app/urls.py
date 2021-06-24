2052e49915e62d7aaf61a5672c286fc883963c20
Jeff-Anderson-C <80008336+Jeff-Anderson-C@users.noreply.github.com>
2021-06-24 16:39:52 -0500

    login and usermanager addded

--- tip_share/tips_app/urls.py
+++ tip_share/tips_app/urls.py
@@ -6,32 +6,33 @@ urlpatterns = [
     path('register', views.register), #call to action = (C2A)
     path('login', views.login), #C2A
     
-    #dashboard - 4 paths (Pool, Group Chat, User Profile, User $ history)
+    # #dashboard - 4 paths (Pool, Group Chat, User Profile, User $ history)
     path('dashboard', views.dashboard), #localhost:8000/dashboard
-    path('logout', views.logout),#C2A
+    # path('logout', views.logout),#C2A
     
-    ## POOL
-    path('pool/start', views.pool_new), #localhost:8000/pool/start
-    path('pool/create', views.pool_create), #C2A
-    path('pool/<int:pool_id>/summary', views.pool), #localhost:8000/pool/3/summary
+    # ## POOL
+    # path('pool/start', views.pool_new), #localhost:8000/pool/start
+    # path('pool/create', views.pool_create), #C2A
+    # path('pool/<int:pool_id>/summary', views.pool), #localhost:8000/pool/3/summary
 
-    #Optional paths not on wireframe:
-    #path('pool/<int:pool_id>/edit',views.pool_edit), #localhost:8000/pool/3/edit
-    #path('pool/<int:pool_id>/delete',views.pool_delete), #C2A
-    #path('pool/<int:pool_id>/update', views.pool_update), #C2A
+    # #Optional paths not on wireframe:
+    # #path('pool/<int:pool_id>/edit',views.pool_edit), #localhost:8000/pool/3/edit
+    # #path('pool/<int:pool_id>/delete',views.pool_delete), #C2A
+    # #path('pool/<int:pool_id>/update', views.pool_update), #C2A
 
-    ## GROUP CHAT
-    path('pool/<int:pool_id>/groupchat', views.groupchat), #localhost:8000/pool/3/groupchat
-    path('pool/<int:pool_id>/groupchat/create', views.groupchat_create), #C2A
-    path('pool/<int:pool_id>/groupchat/user_<int:otherprofile_id>', views.groupchat_user), #localhost:8000/pool/3/groupchat/user_5
-
-    
-    ## USER PROFILE
-    path('user/summary', views.user), #localhost:8000/user/summary
-    path('user/edit',views.user_edit), #localhost:8000/user/edit
-    path('user/update', views.user_update), #C2A
-    
-    ## USER $$ HISTORY 
-    path('transactions/history', views.transactions), #localhost:8000/transaction/history
+    # ## GROUP CHAT
+    # path('pool/<int:pool_id>/groupchat', views.groupchat), #localhost:8000/pool/3/groupchat
+    # path('pool/<int:pool_id>/groupchat/create', views.groupchat_create), #C2A
+    # path('pool/<int:pool_id>/groupchat/user_<int:otherprofile_id>', views.groupchat_user), #localhost:8000/pool/3/groupchat/user_5
 
     
+    # ## USER PROFILE
+    # path('user/summary', views.user), #localhost:8000/user/summary
+    # path('user/edit',views.user_edit), #localhost:8000/user/edit
+    # path('user/update', views.user_update), #C2A
+    
+    # ## USER $$ HISTORY 
+    # path('transactions/history', views.transactions), #localhost:8000/transaction/history
+
+    
+]
--- tip_share/tips_app/views.py
+++ tip_share/tips_app/views.py
@@ -8,64 +8,102 @@ import bcrypt
 ## REGISTER / LOG IN ##
 def index(request):
     #return render(request, 'login.html')
-    return HttpResponse ("This is the loging/register page")
+    return render(request, 'login.html')
+    # return HttpResponse ("This is the loging/register page")
 
+# def register(request):
+#     return redirect('/dashboard')
 def register(request):
-    return redirect('/dashboard')
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        new_user = User.objects.create(
+            first_name = request.POST ['first_name'],
+            last_name = request.POST ['last_name'],
+            email = request.POST ['email'],
+            username = request.POST ['username'],
+            password = pw_hash,
+        )
+        request.session['userid'] = new_user.id 
+        return redirect('/dashboard')
 
 def login(request):
-    return redirect('/dashboard')
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:    
+        user = User.objects.filter(username = request.POST['username'])
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id 
+                return redirect('/dashboard')
+            else:
+                messages.error(request, 'Invalid password')
+            return redirect('/')
+        return redirect('/')
+#     return redirect('/dashboard')
 
-def logout(request):
-    request.session.clear()
-    return redirect('/')
+# def logout(request):
+#     request.session.clear()
+#     return redirect('/')
 
 
-## Dashboard 
+# ## Dashboard 
 def dashboard(request):
-    return HttpResponse ("This is the User Dashboard page displaying pathways to Start Pool, Open Group Chat, View Profile, and View $ History")
+    return render(request, 'dashboard.html')
+#     return HttpResponse ("This is the User Dashboard page displaying pathways to Start Pool, Open Group Chat, View Profile, and View $ History")
 
 
 
-## POOL
-def pool_new(request):
-    return HttpResponse ("User is looking to start a pool")
+# ## POOL
+# def pool_new(request):
+#     return HttpResponse ("User is looking to start a pool")
 
-def pool_create(request, pool_id):
-    return redirect(f'/pool/{pool.id}/summary')
+# def pool_create(request, pool_id):
+#     return redirect(f'/pool/{pool.id}/summary')
 
-def pool(request, pool_id):
-    return HttpResponse ("Reviewing the Summary of the pool information")
+# def pool(request, pool_id):
+#     return HttpResponse ("Reviewing the Summary of the pool information")
 
-#Optional paths not on wireframe:
-#def pool_edit(request, pool_id):
-#def pool_update(request, pool_id):
-#def pool_delete(request, pool_id):
+# #Optional paths not on wireframe:
+# #def pool_edit(request, pool_id):
+# #def pool_update(request, pool_id):
+# #def pool_delete(request, pool_id):
 
 
-## GROUP CHAT    
-def groupchat(request,pool_id):
-    return HttpResponse ("This is the Pool Group Chat Page")
+# ## GROUP CHAT    
+# def groupchat(request,pool_id):
+#     return HttpResponse ("This is the Pool Group Chat Page")
     
-def groupchat_create(request,pool_id):
-    return redirect(f'/pool/{pool.id}/groupchat')
+# def groupchat_create(request,pool_id):
+#     return redirect(f'/pool/{pool.id}/groupchat')
 
-def groupchat_user(request,pool_id):
-    return HttpResponse ("This is another User's Profile page who is in the group chat")
+# def groupchat_user(request,pool_id):
+#     return HttpResponse ("This is another User's Profile page who is in the group chat")
 
 
     
-## USER PROFILE
-def user(request):
-    return HttpResponse ("User Profile Page") 
+# ## USER PROFILE
+# def user(request):
+#     return HttpResponse ("User Profile Page") 
 
-def user_edit(request):
-    return HttpResponse ("This is my EDIT User Profile Page")
+# def user_edit(request):
+#     return HttpResponse ("This is my EDIT User Profile Page")
     
-def user_update(request):
-    return redirect('/user/summary')
+# def user_update(request):
+#     return redirect('/user/summary')
 
-## USER $$ HISTORY
-def transaction(request):
-    return HttpResponse ("This is the Transaction History Page")
+# ## USER $$ HISTORY
+# def transaction(request):
+#     return HttpResponse ("This is the Transaction History Page")
     
