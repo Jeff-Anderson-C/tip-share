a41ef1f369a1a9efb00d93823176a40513aaecec
Jeff-Anderson-C <80008336+Jeff-Anderson-C@users.noreply.github.com>
2021-06-24 19:12:47 -0500

    merging

--- tip-share-master/tips_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from .import views
-
-urlpatterns = [
-    path('', views.index),
-    path('profile', views.user),
-    path('other_profile', views.other_profile)
-]
